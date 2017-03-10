8af839d80f29b86445bbacace176ddbde8db5c98
Timchen7 <timchen7@yahoo.com>
2017-03-10 04:15:59 +0000

    Heroku add-on sendgrid email server installing, need 868

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,18 +64,38 @@ Mailit::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+#------------- using Amazon SES -----------------------------  
 # aws_credentials = Aws::Credentials.new(ENV['AKIAJZK4NBTKD4UMGH3A'], ENV['qs1R4bGGTpCdZdhOgyZ03dvCZdA4NZV3WyJdCLye'])
 # Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['us-east-1'])
 # config.action_mailer.delivery_method = :aws_ses  
+#------------- using mailtrap ----------------------------- 
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+  # :user_name => 'f96254d422f8de',
+  # :password => 'dca8a6de674a29',
+  # :address => 'smtp.mailtrap.io',
+  # :domain => 'smtp.mailtrap.io',
+  # :port => '2525',
+  # :authentication => :cram_md5
+# }
+
+#------------- using SendGrid -----------------------------  
+# https://sendgrid.com/docs/Integrate/Frameworks/rubyonrails.html
+# http://stackoverflow.com/questions/4114835/heroku-devise-missing-host-to-link-to-please-provide-host-parameter-or-set-d
+# https://sendgrid.com/docs/Integrate/index.html?ls=Advertisement&cid=70180000000cl5Z&lsd=adwords&kw=%2Bsendgrid&mt=b&gaid=Integration_Sitelink&mc=Paid%20Search&mcd=AdWords&keyword=%2Bsendgrid&network=g&matchtype=b&mobile=&content=&search=1&cvosrc=PPC.Google.%2Bsendgrid&cvo_cid=Integration_Sitelink&gclid=Cj0KEQiAl4TGBRDhgvmikdHPsdABEiQAtBcc8IpcMCQ16f4IkjZxKGcL_882EXjx2J6ef-fiyuF6bXYaAphu8P8HAQ
+# https://sendgrid.com/docs/Classroom/Troubleshooting/Account_Administration/heroku_users_find_your_sendgrid_credentials.html
+# Use ports 25 or 587 for plain/TLS connections and port 465 for SSL connections
+# https://elements.heroku.com/addons/sendgrid?app=usendemail
+# need to go to Heroku add-ons SendGrid and get sendgrid_user_name & password, which has FREE trial, but Heroku NOT FREE for this add-on
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  :user_name => 'f96254d422f8de',
-  :password => 'dca8a6de674a29',
-  :address => 'smtp.mailtrap.io',
-  :domain => 'smtp.mailtrap.io',
-  :port => '2525',
-  :authentication => :cram_md5
+  :user_name => 'sendgrid_user_name',
+  :password => 'sendgrid_password',
+  :domain => 'https://usendemail.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,  
+  :enable_starttls_auto => true  
 }
 
   # specify what domain to use for mailer URLs
